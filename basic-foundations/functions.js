39939e25071a1c7ab80baf49272f1fe052316133
Bindu <paidihimabindu@gmail.com>
2025-11-11 19:03:20 +0530

    Check point

--- basic-foundations/functions.js
+++ basic-foundations/functions.js
@@ -1,10 +1,24 @@
-//--------- Arrow functions----------
-console.log("Global Scope:", this)
-function greet(){
-    console.log("Greet Scope:", this);
+// console.log("Global Scope:", this)
+// function greet(){
+//     console.log("Greet Scope:", this);
+// }
+// const greetArrow = () => {
+//     console.log("This is an arrow function!")
+// }
+// greet()
+// greetArrow()
+const course = {
+    courseName: "MERN + DSA",
+    read: function(){
+        this.bindu = "batch-4"
+        console.log("this object inside read function:",this)
+    },
+    write: () => {
+        console.log("this object inside write function:",this)
+        setTimeout(() => {
+            console.log(this);
+        },1000)
+    }
 }
-const greetArrow = () => {
-    console.log("This is an arrow function!")
-}
-greet()
-greetArrow()
+course.write()
+course.read()
